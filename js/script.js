bc4917e720e3f60fc728e48ea9365443b217ae7a
Pavel <lol.pavel.08@mail.ru>
2022-10-02 18:05:57 +0500

    Доделан слайдер для первой страницы

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const taskbar_left = document.querySelector('.taskbar-left')
 const grerting = document.querySelector('.greeting')
 const grerting_mob = document.querySelector('.greeting-mob')
 const burger = document.querySelector('.burger')
+const mentor = document.querySelector('.mentor')
 const task = document.querySelector('.task')
 // Отображение селекта в activity
 activ_select.addEventListener('click', function() {
@@ -54,53 +55,108 @@ if (innerWidth<=1375) {
 }
 
 // Работа слайдеров
-// Task
-const task_slider = task.querySelector('.slider-wrapper')
-const task_iteams = Array.from(task_slider.children)
-const sliderBtn = Array.from(task.querySelectorAll('.slider-btn'))
-task_iteams.forEach(function (slide, index) {
-    if (index !== 0) {
-        slide.classList.add('_hidden')
-    }
-    slide.dataset.index = index
-    task_iteams[0].setAttribute('data-active', '')
-})
+// Task and mentors
+if (innerWidth<=512) {
+    // Task
+    const task_slider = task.querySelector('.slider-wrapper')
+    const task_iteams = Array.from(task_slider.children)
+    const sliderBtn = Array.from(task.querySelectorAll('.slider-btn'))
+    task_iteams.forEach(function (slide, index) {
+        if (index !== 0) {
+            slide.classList.add('_hidden')
+        }
+        slide.dataset.index = index
+        task_iteams[0].setAttribute('data-active', '')
+    })
 
-sliderBtn.forEach(function(iteam,index) {
-    if (index==0) {
-        iteam.addEventListener('click', function() {
-            let activeSlide = task.querySelector('[data-active]')
-            let i = +activeSlide.dataset.index -1
+    sliderBtn.forEach(function(iteam,index) {
+        if (index==0) {
+            iteam.addEventListener('click', function() {
+                let activeSlide = task.querySelector('[data-active]')
+                let i = +activeSlide.dataset.index -1
 
-            if (i<0) {
-                i = task_iteams.length - 1
-            }
+                if (i<0) {
+                    i = task_iteams.length - 1
+                }
 
-            let nextSlide = task.querySelector(`[data-index = '${i}']`)
+                let nextSlide = task.querySelector(`[data-index = '${i}']`)
 
-            activeSlide.classList.add('_hidden')
-            activeSlide.removeAttribute('data-active')
+                activeSlide.classList.add('_hidden')
+                activeSlide.removeAttribute('data-active')
 
-            nextSlide.classList.remove('_hidden')
-            nextSlide.setAttribute('data-active', '')
-        })
-    }
-    else {
-        iteam.addEventListener('click', function() {
-            let activeSlide = task.querySelector('[data-active]')
-            let i = +activeSlide.dataset.index +1
+                nextSlide.classList.remove('_hidden')
+                nextSlide.setAttribute('data-active', '')
+            })
+        }
+        else {
+            iteam.addEventListener('click', function() {
+                let activeSlide = task.querySelector('[data-active]')
+                let i = +activeSlide.dataset.index +1
 
-            if (i==task_iteams-1) {
-                i = 0
-            }
+                if (i == task_iteams.length) {
+                    i = 0
+                }
 
-            let nextSlide = task.querySelector(`[data-index = '${i}']`)
+                let nextSlide = task.querySelector(`[data-index = '${i}']`)
 
-            activeSlide.classList.add('_hidden')
-            activeSlide.removeAttribute('data-active')
+                activeSlide.classList.add('_hidden')
+                activeSlide.removeAttribute('data-active')
 
-            nextSlide.classList.remove('_hidden')
-            nextSlide.setAttribute('data-active', '')
-        })
-    }
-})
+                nextSlide.classList.remove('_hidden')
+                nextSlide.setAttribute('data-active', '')
+            })
+        }
+    })
+
+    // Mentor
+
+    const mentor_slider = mentor.querySelector('.slider-wrapper')
+    const mentor_iteams = Array.from(mentor_slider.children)
+    const sliderBtnM = Array.from(mentor.querySelectorAll('.slider-btn'))
+    mentor_iteams.forEach(function (slide, index) {
+        if (index !== 0) {
+            slide.classList.add('_hidden')
+        }
+        slide.dataset.index = index
+        mentor_iteams[0].setAttribute('data-active', '')
+    })
+
+    sliderBtnM.forEach(function(iteam,index) {
+        if (index==0) {
+            iteam.addEventListener('click', function() {
+                let activeSlide = mentor.querySelector('[data-active]')
+                let i = +activeSlide.dataset.index -1
+
+                if (i<0) {
+                    i = mentor_iteams.length - 1
+                }
+
+                let nextSlide = mentor.querySelector(`[data-index = '${i}']`)
+
+                activeSlide.classList.add('_hidden')
+                activeSlide.removeAttribute('data-active')
+
+                nextSlide.classList.remove('_hidden')
+                nextSlide.setAttribute('data-active', '')
+            })
+        }
+        else {
+            iteam.addEventListener('click', function() {
+                let activeSlide = mentor.querySelector('[data-active]')
+                let i = +activeSlide.dataset.index +1
+
+                if (i == task_iteams.length) {
+                    i = 0
+                }
+
+                let nextSlide = mentor.querySelector(`[data-index = '${i}']`)
+
+                activeSlide.classList.add('_hidden')
+                activeSlide.removeAttribute('data-active')
+
+                nextSlide.classList.remove('_hidden')
+                nextSlide.setAttribute('data-active', '')
+            })
+        }
+    })
+}
